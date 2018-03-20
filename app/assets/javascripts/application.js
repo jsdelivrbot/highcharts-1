e167912f60786db816f046713cb3e3db83666b4b
Jack Reuter <djreuter45@gmail.com>
2018-03-20 10:14:47 -0600

    converted from Sinatra to Rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,87 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require jquery_ujs
+var xCatagories;
+var dataValues;
+
+$(document).ready(function() {
+  $('#test-chart').on('submit', function(e) {
+    e.preventDefault();
+    var options = {
+      title: {
+        text: $('#chart-name').val()
+      },
+      chart: {
+        renderTo: 'chart-container',
+        type: $('#chart-type').val()
+      },
+      xAxis: {
+        categories: xCatagories
+      },
+      yAxis: {
+        title: {
+          text: $('#yAxisTitle').val()
+        }
+      },
+      series: [{
+        name: $('#dataName').val(),
+        data: dataValues
+      }]
+    }
+    dataValues = $.map($('#dataValues').val().split(', '), function(value) {
+      return parseInt(value);
+    });
+    xCatagories = $('#xCatagories').val();
+    xCatagories = xCatagories.split(', ');
+    var chart = new Highcharts.Chart(options);
+  });
+
+  $('#moreFields').click(function() {
+    $('.data-fields').append("<div class='item'>Data Name: <input type='text'> </div><div class='item'> Data Values: <input type='text'></div>")
+  });
+});
+
+// $(function() {
+//   var csv = Papa.parse(document.getElementById('csv-data').innerHTML);
+//   $('#chart-container').highcharts({
+//     chart: {
+//       type: 'column'
+//     },
+//     data: {
+//       rows: csv.data,
+//       seriesMapping: [{
+//         label: 2
+//       }]
+//     },
+//     title: {
+//       text: 'Pollen Count'
+//     },
+//     xAxis: {
+//       minTickInterval: 24 * 36e5
+//     },
+//     yAxis: {
+//       title: {
+//         text: 'Pollen level'
+//       },
+//       labels: {
+//         format: '{value} g'
+//       }
+//     },
+//     legend: {
+//       enabled: false
+//     },
+//     plotOptions: {
+//       series: {
+//         dataLabels: {
+//           enabled: true,
+//           format: '{point.label}'
+//         },
+//         tooltip: {
+//           valueSuffix: ' g'
+//         }
+//       }
+//     }
+//   });
+// });
